1565f6bbc082d11bbf3ae03bc8ca3f9365b1ca3e
RohanChopra <rohanchopra2005@gmail.com>
2025-06-22 17:28:39 -0500

    Added images

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ const App = () => {
         <header>
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>  
         </header>  
+
+        <p>Search</p>
       </div> 
     </main>
   )
